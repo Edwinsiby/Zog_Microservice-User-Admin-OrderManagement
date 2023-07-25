ef9bafb8cd5722e629f4003fe9ad57ec825f427f
edwinsiby <edwinsibyrajakumary@gmail.com>
2023-07-25 18:57:12 +0530

    dns injected

--- pkg/db/database.go
+++ pkg/db/database.go
@@ -3,7 +3,6 @@ package db
 import (
 	"fmt"
 	"service4/pkg/entity"
-	"service4/pkg/utils"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -12,8 +11,8 @@ import (
 var DB *gorm.DB
 
 func ConnectToDB() (*gorm.DB, error) {
-	config, err := utils.LoadConfig("./")
-	db, err := gorm.Open(postgres.Open(config.DSN), &gorm.Config{})
+	// config, err := utils.LoadConfig("./")
+	db, err := gorm.Open(postgres.Open("host=db user=edwin dbname=edwin password=acid port=5432 sslmode=disable"), &gorm.Config{})
 	if err != nil {
 		return nil, fmt.Errorf("failed to connect to database: %w", err)
 	}
